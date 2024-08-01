4063556d325d2816281cf270cea912e758dedfcf
Aira Jin <aira_garganera@dlsu.edu.ph>
2024-08-01 15:58:51 +0800

    store, dir functions added

---
+++ Clients/client.py
@@ -0,0 +1,103 @@
+import socket 
+import argparse
+
+def join_action(sock, host, port):
+
+    # Connecting with Server 
+    sock.connect((host, int(port))) 
+    print("Join command executed!")
+    
+def leave_action(sock):
+    sock.close()
+    print("Leave command executed!")
+
+def register_action():
+    print("Register command executed!")
+
+def storeFile(fileName):
+    try: 
+    # Reading file and sending data to server 
+        file = open(fileName, "r") 
+        data = file.read()
+        if not data:
+            print("Nothing here.")
+        while data: 
+                sock.send(str(data).encode()) 
+                data = file.read() 
+            # File is closed after data is sent 
+        file.close()
+
+    except IOError: 
+        print('You entered an invalid filename! Please enter a valid name') 
+
+    print("Store command executed!")
+
+def dir(sock):
+    # Send a request to the server to list the directory
+    request = "LIST_DIR"
+    sock.sendall(request.encode())
+
+    # Receive the response from the server
+    # Assume the server sends a single string with file names separated by newline characters
+    response = sock.recv(4096).decode()
+
+    # Print the received list of files
+    print("Files in the server directory:")
+    print(response)
+    print("Dir command executed!")
+
+
+# Creating Client Socket 
+if __name__ == '__main__': 
+
+    # host = '127.0.0.1'
+    # port = 8080
+    userHandle = "ClientX"
+  
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+    while True: 
+        
+        clientinput = input("").strip().split()
+        command = clientinput[0]
+
+        if command == '/join':
+            print(command)
+            join_action(sock, clientinput[1], clientinput[2])
+
+        elif command == '/leave':
+            leave_action(sock)
+
+        elif command == '/register':
+            userHandle = clientinput[1]
+            print("Your userHandle is: ", userHandle)
+            print("Register command executed!")
+
+        elif command == '/store':
+            storeFile(clientinput[1])
+
+        elif command == '/dir':
+            dir(sock)
+
+        elif command == '/help':
+            print("Available commands:\n/join - Executes the join action\n/hello - Executes the hello action\n/exit - Exits the program\n/help - Shows this help message")
+        else:
+            print("Unknown command. Type /help for a list of available commands.")
+
+'''
+        filename = input('Input filename you want to send: ') 
+        try: 
+           # Reading file and sending data to server 
+            fi = open(filename, "r") 
+            data = fi.read() 
+            if not data: 
+                break
+            while data: 
+                sock.send(str(data).encode()) 
+                data = fi.read() 
+            # File is closed after data is sent 
+            fi.close() 
+
+        except IOError: 
+            print('You entered an invalid filename! Please enter a valid name') 
+'''
---
+++ Server/server.py
@@ -0,0 +1,72 @@
+import socket 
+import os
+
+# def handle_client(conn):
+#         # Receive request from the client
+#         request = conn.recv(1024).decode()
+#         if request == "LIST_DIR":
+#             # Get the list of files in the current directory
+#             files = os.listdir(".")
+#             # Join the list into a single string, separated by newlines
+#             response = "\n".join(files)
+#             # Send the response back to the client
+#             conn.sendall(response.encode())
+#             # Add additional command handling as needed
+  
+if __name__ == '__main__': 
+    # Defining Socket 
+    host = '127.0.0.1'
+    port = 8080
+    totalclient = 1
+    
+    # int(input('Enter number of clients: ')) 
+  
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    sock.bind((host, port)) 
+    sock.listen(totalclient) 
+    # Establishing Connections 
+    connections = [] 
+    print('Initiating clients') 
+    for i in range(totalclient): 
+        conn = sock.accept() 
+        connections.append(conn) 
+        print('Connected with client', i+1)
+  
+    fileno = 0
+    idx = 0
+    for conn in connections: 
+        # Receiving File Data 
+        idx += 1
+        print(conn)
+        
+        data = conn[0].recv(1024).decode() 
+        if data == "LIST_DIR":
+            print('Getting directory')
+            files = [f for f in os.listdir(".") if f.endswith(".txt")]
+            response = "\n".join(files)
+            conn[0].send(response.encode())
+
+        if not data: 
+            continue
+    # Creating a new file at server end and writing the data 
+        filename = 'output'+str(fileno)+'.txt'
+        fileno = fileno+1
+        fo = open(filename, "w") 
+        while data: 
+            if not data: 
+                break
+            else: 
+                fo.write(data) 
+                data = conn[0].recv(1024).decode() 
+
+        print() 
+        print('Receiving file from client', idx) 
+        print() 
+        print('Received successfully! New filename is:', filename)
+
+
+        fo.close() 
+    # Closing all Connections 
+    for conn in connections: 
+        conn[0].close() 
+
